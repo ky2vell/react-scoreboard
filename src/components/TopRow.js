60100c8b51383d7d8557bac0e480885916412f14
Kyle Lovell <ky2vell@gmail.com>
2020-04-23 07:59:54 -0400

    Stretch: added styling

--- src/components/TopRow.js
+++ src/components/TopRow.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "../App.css";
+import eagles from "../images/Eagles.png";
+import lions from "../images/Lions.png";
 
 const TopRow = (props) => {
     return (
         <div className="topRow">
             <div className="home">
-                <h2 className="home__name">Lions</h2>
+                <img className="team__logo" src={lions} alt="Home Team Logo" />
                 <div className="home__score">{props.homeScore}</div>
             </div>
             <div className="timer">00:03</div>
             <div className="away">
-                <h2 className="away__name">Tigers</h2>
+                <img className="team__logo" src={eagles} alt="Home Team Logo" />
                 <div className="away__score">{props.awayScore}</div>
             </div>
         </div>
